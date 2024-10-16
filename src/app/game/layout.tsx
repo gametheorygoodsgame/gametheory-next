6f1c69c57076cbc364c3f6a6e48c4357baa23e9e
l3nn4rt02 <147611731+l3nn4rt02@users.noreply.github.com>
2024-10-16 09:25:18 +0200

    Update layout.tsx
    jsdoc try

--- src/app/game/layout.tsx
+++ src/app/game/layout.tsx
@@ -4,6 +4,17 @@ import { AppShell } from '@mantine/core';
 import React from 'react';
 import PlayerMenuBar from '../../components/menuBars/PlayerMenuBar';
 
+/**
+ * This function defines a layout component for student pages in a TypeScript React application.
+ * @param  - The `StudentLayout` function is a React component that serves as a layout for
+ * student-related pages. It takes a `children` prop, which represents the content that will be
+ * rendered within the layout. The layout consists of an `AppShell` component with a header containing
+ * a `PlayerMenuBar` component
+ * @returns The `StudentLayout` function is returning a JSX structure that includes an `AppShell`
+ * component with a `Header` and a `Main` component. Inside the `Header`, a `PlayerMenuBar` component
+ * is rendered. The `Main` component has a background color set to white and renders the `children`
+ * passed to the `StudentLayout` component.
+ */
 export default function StudentLayout({ children }: { children: React.ReactNode }) {
   return (
       <>
