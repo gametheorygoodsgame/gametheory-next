cbf4df27ea324752fc7da8f5cd06d8711aa018f9
Leon <leonascione@gmail.com>
2025-05-15 13:06:26 +0200

    übersprungene Spieler werden dem Spielleiter optisch angezeigt

--- src/components/playerList/playerList.tsx
+++ src/components/playerList/playerList.tsx
@@ -12,31 +12,15 @@ export interface DataCollectionItem {
   isMoveMade: boolean;
 }
 
-/**
- * A component that displays a list of players in the game and their current turn status.
- * The list shows the number of players who have finished their move and updates the status for each player.
- *
- * @param {Object} props - The component props.
- * @param {Game | undefined} props.game - The game object that contains player data and game state.
- * 
- * @returns {JSX.Element} A list of players with their respective move status.
- *
- */
-export default function PlayerList({ game }: { game: Game | undefined; }) {
+export default function PlayerList({ game }: { game: Game | undefined }) {
   const icon = <IconCards />;
   const waiting = <IconClockHour3 />;
   const success = <IconCircleCheck />;
 
-  /**
-   * Returns the number of players who have finished their moves for the current turn.
-   * 
-   * @returns {number} The count of players who have made their move for the current turn.
-   */
   function getNumFinishedPlayers() {
     let count = 0;
     game?.players.forEach((player) => {
       if (player.moves[game?.currentTurn]) {
-        // eslint-disable-next-line no-plusplus
         count++;
       }
     });
@@ -44,25 +28,42 @@ export default function PlayerList({ game }: { game: Game | undefined; }) {
   }
 
   return (
-    <Stack>
-      <Text fw={700}>
-        {getNumFinishedPlayers()} / {game ?
-          game.players.length : 0} Spieler haben ihren Zug beendet.
-      </Text>
-      {game?.players.map((player: Player) => (
-        <Badge
-          leftSection={icon}
-          rightSection={player.moves.length - 1 >= game?.currentTurn ? success : waiting}
-          h={50}
-          w={200}
-          size="xl"
-          radius="md"
-          color={player.moves.length - 1 >= game?.currentTurn ? 'brand.0' : 'brand.7'}
-          key={player.id}
-        >
-          {player.name}
-        </Badge>
-      ))}
-    </Stack>
+      <Stack>
+        <Text fw={700}>
+          {getNumFinishedPlayers()} / {game ? game.players.length : 0} Spieler haben ihren Zug beendet.
+        </Text>
+
+        {game?.players.map((player: Player) => {
+          const isInactive =
+              (player as any).inactiveSinceTurn !== -1;
+
+          const playerNameStyle = {
+            textDecoration: isInactive ? 'line-through' : 'none',
+            color: isInactive ? '#999' : undefined,
+          };
+
+          return (
+              <Badge
+                  leftSection={icon}
+                  rightSection={
+                    player.moves.length - 1 >= game?.currentTurn ? success : waiting
+                  }
+                  h={50}
+                  w={200}
+                  size="xl"
+                  radius="md"
+                  color={
+                    player.moves.length - 1 >= game?.currentTurn
+                        ? 'brand.0'
+                        : 'brand.7'
+                  }
+                  key={player.id}
+                  style={playerNameStyle}
+              >
+                {player.name}
+              </Badge>
+          );
+        })}
+      </Stack>
   );
 }
